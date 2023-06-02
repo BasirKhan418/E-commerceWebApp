ac7dade198343ee7bbb7e7f1118b728e128e2da4
Basir Khan <khanbasir5555@gmail.com>
2023-06-02 20:24:25 +0530

    adding backend and filtering features

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
-
-const slug = ({addToCart}) => {
+import Product from '@/models/Product';
+import mongoose from 'mongoose';
+const slug = ({addToCart,product,variants}) => {
     const router =useRouter()
     const {slug}=router.query
     const [pin,Setpin]=useState()
@@ -21,7 +22,15 @@ const slug = ({addToCart}) => {
     const onChange=(e)=>{
       Setpin(e.target.value);
     }
-  return (
+    const[color,setColor]=useState(product.color)
+    const[size,setSize]=useState(product.size)
+    const refreshVariants=(newsize,newcolor)=>{
+      console.log("color is",newcolor,"size",newsize);
+      console.log("variants is",variants)
+     let url=`http://localhost:3000/products/${variants[newcolor][newsize]['slug']}`;
+     window.location=url;
+    }
+    return(
     <div>
      <section className="text-gray-600 body-font overflow-hidden bg-white">
   <div className="container px-5 py-16 mx-auto">
@@ -70,19 +79,25 @@ const slug = ({addToCart}) => {
         <p className="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
         <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
           <div className="flex">
-            <span className="mr-3">Color</span>
-            <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button className="border-2 border-gray-300 ml-1 bg-pink-500 rounded-full w-6 h-6 focus:outline-none"></button>
+            <span className="mr-3">Color</span> 
+            {Object.keys(variants).includes('white')&& Object.keys(variants['white']).includes(size)&&<button onClick={()=>{refreshVariants(size,"white")}} className={`border-2 bg-white rounded-full w-6 h-6 focus:outline-none ${color==="white"?"border-black":"border-grey-300"}`}></button>}
+            {Object.keys(variants).includes('black')&&Object.keys(variants['black']).includes(size)&&<button onClick={()=>{refreshVariants(size,"black")}} className={`border-2 bg-neutral-950 rounded-full w-6 h-6 focus:outline-none ${color==="black"?"border-red":"border-grey-300"}`}></button>}
+            {Object.keys(variants).includes('red')&&Object.keys(variants['red']).includes(size)&&<button onClick={()=>{refreshVariants(size,"red")}} className={`border-2 bg-red-700 rounded-full w-6 h-6 focus:outline-none ${color==="red"?"border-black":"border-grey-300"}`}></button>}
+            {Object.keys(variants).includes('blue')&&Object.keys(variants['blue']).includes(size)&&<button onClick={()=>{refreshVariants(size,"blue")}} className={`border-2 bg-blue-700 rounded-full w-6 h-6 focus:outline-none ${color==="blue"?"border-black":"border-grey-300"}`}></button>}
+            {Object.keys(variants).includes('green')&&Object.keys(variants['green']).includes(size)&&<button onClick={()=>{refreshVariants(size,"green")}} className={`border-2 bg-green-700 rounded-full w-6 h-6 focus:outline-none ${color==="green"?"border-black":"border-grey-300"}`}></button>}
+            {Object.keys(variants).includes('pink')&&Object.keys(variants['pink']).includes(size)&&<button onClick={()=>{refreshVariants(size,"pink")}} className={`border-2 bg-pink-700 rounded-full w-6 h-6 focus:outline-none ${color==="pink"?"border-black":"border-grey-300"}`}></button>}
+            {Object.keys(variants).includes('yellow')&&Object.keys(variants['yellow']).includes(size)&&<button onClick={()=>{refreshVariants(size,"yellow")}} className={`border-2 bg-yellow-700 rounded-full w-6 h-6 focus:outline-none ${color==="yellow"?"border-black":"border-grey-300"}`}></button>}
           </div>
           <div className="flex ml-6 items-center">
             <span className="mr-3">Size</span>
             <div className="relative">
-              <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-pink-200 focus:border-pink-500 text-base pl-3 pr-10">
-                <option>SM</option>
-                <option>M</option>
-                <option>L</option>
-                <option>XL</option>
+              <select value={size} onChange={(e)=>{refreshVariants(e.target.value,color)}} className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-pink-200 focus:border-pink-500 text-base pl-3 pr-10">
+                {Object.keys(variants[color]).includes('S')&&<option value={'S'}>S</option>}
+                {Object.keys(variants[color]).includes('M')&&<option value={'M'}>M</option>}
+                {Object.keys(variants[color]).includes('L')&&<option value={'L'}>L</option>}
+                {Object.keys(variants[color]).includes('XL')&&<option value={'XL'}>XL</option>}
+                {Object.keys(variants[color]).includes('XXL')&&<option value={'XXL'}>XXL</option>}
+                {Object.keys(variants[color]).includes('SM')&&<option value={'SM'}>SM</option>}
               </select>
               <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
@@ -118,6 +133,26 @@ const slug = ({addToCart}) => {
 </section>
     </div>
   )
+}
+export async function getServerSideProps(context){
+  if(!mongoose.connections[0].readyState){
+    await mongoose.connect(process.env.MONGO_URI);
+}
+    let product= await Product.findOne({slug:context.query.slug})
+    let variants = await Product.find({title: product.title})
+    let colorSizeSlug={}
+    for (let item of variants){
+      if(Object.keys(colorSizeSlug).includes(item.color)){
+        colorSizeSlug[item.color][item.size]={slug:item.slug}
+      }
+      else{
+        colorSizeSlug[item.color]={}
+        colorSizeSlug[item.color][item.size]={slug:item.slug}
+      }
+    }
+  return{
+    props:{product:JSON.parse(JSON.stringify(product)),variants:JSON.parse(JSON.stringify(colorSizeSlug))}
+  }
 }
 
 export default slug
